dc73b75526f67a96f366a58e1899abf58fdc23ad
incentt <wisnatavincent@gmail.com>
2021-04-18 18:00:39 +0700

    first commit

---
+++ Assets/Script/CameraMoveController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveController : MonoBehaviour
+{
+    [Header("Position")]
+    public Transform player;
+    public float horizontalOffset;
+
+    private void Update()
+    {
+        gameObject.transform.position = new Vector2(player.transform.position.x + horizontalOffset, gameObject.transform.position.y);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Script/CharacterMove.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+    private bool isJumping;
+    private bool isOnGround;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+
+    public AudioClip jump;
+    private AudioSource audioPlayer;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+
+    private Rigidbody2D rig;
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocityVector = rig.velocity;
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+        rig.velocity = velocityVector;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+                anim.SetBool("IsGround", true);
+                anim.SetBool("IsJumping", false);
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rig.velocity = velocityVector;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+                anim.SetBool("IsJumping", true);
+                anim.SetBool("IsGround", false);
+                audioPlayer.PlayOneShot(jump);
+            }
+        }
+        
+    }
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+
+
+
+}
